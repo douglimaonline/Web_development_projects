f6054f5c0270219c7389be8debf705134628cec2
Douglas <douglimaonline@gmail.com>
2023-10-20 18:42:50 -0300

    Scroll Animations

--- MyWebSite/index.js
+++ MyWebSite/index.js
@@ -1,3 +1,5 @@
+
+// Function to change the background color
 function changeColor() {
     if (document.firstElementChild.getAttribute("data-bs-theme") === "dark") {
 
@@ -7,15 +9,42 @@ function changeColor() {
         document.querySelector("#GitHub-Infos #GitHub-Status").setAttribute("src", "https://github-readme-stats.vercel.app/api?username=douglimaonline&hide=contribs&show_icons=true&theme=transparent&hide_border=true&title_color=000000&hide_rank=true&custom_title=Estatísticas do GitHub&card_width=320&locale=pt-br")
         document.querySelector("#GitHub-Infos #GitHub-Lenguages").setAttribute("src", "https://github-readme-stats.vercel.app/api/top-langs/?username=douglimaonline&layout=donut&theme=transparent&hide_border=true&title_color=000000&include_all_commits=true&langs_count=7&custom_title=Linguagens mais usadas&hide=Rich+Text+Format,html")
 
+
     } else {
 
         document.firstElementChild.setAttribute("data-bs-theme", "dark");
         document.querySelector("#header").firstElementChild.setAttribute("class", "container border-bottom fixed-top bg-dark")
-        document.querySelector("#header #ChangeCollorButton").firstElementChild.setAttribute("class", "form-check-input bg-dark")
+        document.querySelector("#header #ChangeCollorButton").firstElementChild.setAttribute("class", "form-check-input bg-dark opacity-75")
         document.querySelector("#GitHub-Infos #GitHub-Status").setAttribute("src", "https://github-readme-stats.vercel.app/api?username=douglimaonline&hide=contribs&show_icons=true&theme=transparent&hide_border=true&title_color=FFFFFF&hide_rank=true&custom_title=Estatísticas do GitHub&card_width=320&locale=pt-br")
         document.querySelector("#GitHub-Infos #GitHub-Lenguages").setAttribute("src", "https://github-readme-stats.vercel.app/api/top-langs/?username=douglimaonline&layout=donut&theme=transparent&hide_border=true&title_color=FFFFFF&include_all_commits=true&langs_count=7&custom_title=Linguagens mais usadas&hide=Rich+Text+Format,html")
     }
 
 }
-
 document.querySelector("#ChangeCollorButton").onclick = changeColor;
+
+// Change the switch color position
+if (window.innerWidth >= 992) {
+    document.querySelector("#header #ChangeCollorButton").setAttribute("class", "form-switch  align-self-center")
+}
+
+// const observer = new IntersectionObserver((entries) =› {
+// entries.forEach( (entry) =› { console. log (entry)
+// if (entry.isIntersecting) {
+// entry.target.classList.add( 'show");
+// } else {
+// entry.target.classList.remove(' show');
+// }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classlist.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
