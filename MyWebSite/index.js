6f55d728c28ad2d8a0964127d2a852b5d471d6f9
Douglas <douglimaonline@gmail.com>
2023-10-24 17:11:34 -0300

    MyWebSite upload

--- MyWebSite/index.js
+++ MyWebSite/index.js
@@ -1,4 +1,3 @@
-
 // Function to change the background color
 function changeColor() {
     if (document.firstElementChild.getAttribute("data-bs-theme") === "dark") {
@@ -8,10 +7,7 @@ function changeColor() {
         document.querySelector("#header #ChangeCollorButton").firstElementChild.setAttribute("class", "form-check-input bg-light opacity-75")
         document.querySelector("#GitHub-Infos #GitHub-Status").setAttribute("src", "https://github-readme-stats.vercel.app/api?username=douglimaonline&hide=contribs&show_icons=true&theme=transparent&hide_border=true&title_color=000000&hide_rank=true&custom_title=Estatísticas do GitHub&card_width=320&locale=pt-br")
         document.querySelector("#GitHub-Infos #GitHub-Lenguages").setAttribute("src", "https://github-readme-stats.vercel.app/api/top-langs/?username=douglimaonline&layout=donut&theme=transparent&hide_border=true&title_color=000000&include_all_commits=true&langs_count=7&custom_title=Linguagens mais usadas&hide=Rich+Text+Format,html")
-
-
     } else {
-
         document.firstElementChild.setAttribute("data-bs-theme", "dark");
         document.querySelector("#header").firstElementChild.setAttribute("class", "container border-bottom fixed-top bg-dark")
         document.querySelector("#header #ChangeCollorButton").firstElementChild.setAttribute("class", "form-check-input bg-dark opacity-75")
@@ -27,14 +23,7 @@ if (window.innerWidth >= 992) {
     document.querySelector("#header #ChangeCollorButton").setAttribute("class", "form-switch  align-self-center")
 }
 
-// const observer = new IntersectionObserver((entries) =› {
-// entries.forEach( (entry) =› { console. log (entry)
-// if (entry.isIntersecting) {
-// entry.target.classList.add( 'show");
-// } else {
-// entry.target.classList.remove(' show');
-// }
-
+// Add the fade in animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -45,6 +34,7 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Zoom animation
