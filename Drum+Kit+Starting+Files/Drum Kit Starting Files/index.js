f2f25dd12bdeff8dc0b25f594cf6718dba7cc77d
douglas <douglimaonline@gmail.com>
2023-10-29 20:31:50 -0300

    drum kit update

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,9 +1,22 @@
 const drums = document.querySelectorAll(".drum");
+var crash = new Audio("sounds/crash.mp3");
+var kick = new Audio("sounds/crash.mp3");
+var snare = new Audio("sounds/crash.mp3");
+var tom1 = new Audio("sounds/crash.mp3");
+var tom2 = new Audio("sounds/crash.mp3");
+var tom3 = new Audio("sounds/crash.mp3");
+var tom4 = new Audio("sounds/crash.mp3");
+
+var drumsound = {
+    "w": "crash",
+    "a": "kick",
+    "s": "snare",
+    "d": "tom1,"
+};
 
 drums.forEach(drum => {
     drum.addEventListener("click", function () {
-        if (drum.getAttribute("class".slice(0,1) === "w")) {
-            alert("w clicked.");
-        }
-    })
+        console.log(this.textContent);
+    });
 });
+
